0d94371dbd1240689c346a65b99d1bec67ae670b
VictoriaGailitis <105845684+VictoriaGailitis@users.noreply.github.com>
2024-08-23 00:15:53 +0300

    get product by id added

--- main.py
+++ main.py
@@ -6,8 +6,19 @@ app = FastAPI()
 
 @app.get("/products")
 def get_products():
-    query = Product.select().order_by(Product.product_id).dicts()
-    return {'products':list(query)}
+    try:
+        query = Product.select().order_by(Product.product_id).dicts()
+        return {'products':list(query)}
+    except:
+        return {'error': "no records in table"}
+
+@app.get("/products/{product_id}")
+def get_product(product_id : int):
+    try:
+        query = Product.get_by_id(product_id)
+        return {'product': query.__data__}
+    except:
+        return {'error': "item does not exist"}
 
 if __name__ == "__main__":
     uvicorn.run(app)
